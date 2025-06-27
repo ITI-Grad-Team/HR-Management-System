46248e75c8606807298ee16c8da461603388126d
ahmed-elsabbagh778 <ahmedelsabbagh973@gmail.com>
2025-06-27 18:46:35 +0300

    configure supabase

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.jdfshcpcxiefhgsgjcne',
+        'PASSWORD': 'As123456HR',
+        'HOST': 'aws-0-eu-north-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
+
 
 
 # Password validation
