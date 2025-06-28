2c97b0689192b041ea157f39a7f4c3e80420503f
ahmed-elsabbagh778 <ahmedelsabbagh973@gmail.com>
2025-06-28 21:18:59 +0300

    created api for employee data update by hr

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,6 +5,11 @@ from .models import (
     InterviewQuestion, OvertimeClaim, Task, File,
     Position, Report
 )
+from .models import Employee, HoliOrOnlineDayWeekday, HoliOrOnlineDayYearday
+from django.utils import timezone
+from django.core.mail import send_mail
+import string, random
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -71,3 +76,63 @@ class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
         fields = '__all__'
+
+
+class EmployeeDataSerializer(serializers.ModelSerializer):
+    weekdays = serializers.ListField(child=serializers.CharField(), write_only=True, required=False)
+    yeardays = serializers.ListField(
+        child=serializers.DictField(child=serializers.IntegerField()), write_only=True, required=False
+    )
+    password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = Employee
+        fields = [
+            'id',
+            'basic_salary',
+            'overtime_hour_salary',
+            'shorttime_hour_penalty',
+            'absence_penalty',
+            'expected_attend_time',
+            'expected_leave_time',
+            'weekdays',
+            'yeardays',
+        ]
+
+    def update(self, instance, validated_data):
+        weekdays = validated_data.pop('weekdays', [])
+        yeardays = validated_data.pop('yeardays', [])
+        password = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        user = instance.user
+        user.set_password(password)
+        user.save()
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+
+        instance.join_date = timezone.now()
+        instance.save()
+
+        
+
+        send_mail(
+            subject='Welcome to Tempohr',
+            message=f"Your account has been created.\nUsername: {user.username}\nPassword: {password}",
+            from_email='tempohr44@gmail.com',
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+
+        for weekday in weekdays:
+            day_obj, _ = HoliOrOnlineDayWeekday.objects.get_or_create(weekday=weekday)
+            day_obj.employees.add(instance)
+
+        for yearday in yeardays:
+            month = yearday.get('month')
+            day = yearday.get('day')
+            if month and day:
+                day_obj, _ = HoliOrOnlineDayYearday.objects.get_or_create(month=month, day=day)
+                day_obj.employees.add(instance)
+
+        return instance
