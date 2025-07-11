eabc57e40463a848c2468510396d3cfa7edc9b47
Othman Ahmed <othmanahmed258@gmail.com>
2025-07-11 14:38:17 +0300

    pulling latest changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
->>>>>>> ad19e8acd3a0ccef3dd1c250cbdb16c6c95e6afd
