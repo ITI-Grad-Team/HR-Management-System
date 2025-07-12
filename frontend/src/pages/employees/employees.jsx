5ceff3df92daaed3e371f0b8ededab440a5acef5
Othman Ahmed <othmanahmed258@gmail.com>
2025-07-12 19:30:48 +0300

    fetch the employees and apply the filters

--- frontend/src/pages/employees/employees.jsx
+++ frontend/src/pages/employees/employees.jsx
@@ -1,18 +1,39 @@
 import "./employees.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { positions } from "../../lib/Positions.js";
 import { regions } from "../../lib/Regions.js";
 import { useAuth } from "../../context/AuthContext";
+import axiosInstance from "../../api/config.js";
+import BioCard from "../../components/BioCard/BioCard.jsx";
 
 const Directories = () => {
+  const [employees, setEmployees] = useState([]);
   const [positionSelect, setPositionSelect] = useState("");
   const [regionSelect, setRegionSelect] = useState("");
   const [isCoordinatorValue, setIsCoordinatorValue] = useState(false);
   const { role } = useAuth();
 
-  console.log("Selected Position:", positionSelect);
-  console.log("Selected Region:", regionSelect);
-  console.log("Selected IsCoordinatorValue:", isCoordinatorValue);
+  useEffect(() => {
+    axiosInstance
+      .get("/admin/employees/?interview_state=accepted")
+      .then((res) => setEmployees(res.data.results))
+      .catch((err) => console.error(err));
+    console.log(employees);
+  }, [employees]);
+
+  const filteredEmployees = employees.filter((employee) => {
+    const matchesPosition = positionSelect
+      ? employee.position === positionSelect
+      : true;
+    const matchesRegion = regionSelect
+      ? employee.region === regionSelect
+      : true;
+    const matchesCoordinator = isCoordinatorValue
+      ? employee.isCoordinator === isCoordinatorValue
+      : true;
+
+    return matchesPosition && matchesRegion && matchesCoordinator;
+  });
 
   return (
     <div className="directories d-flex">
@@ -40,6 +61,8 @@ const Directories = () => {
             <h2>Employees</h2>
 
             <div className="filter-controls">
+              <span>Filters : </span>
+
               <div className="position-select">
                 <select onChange={(e) => setPositionSelect(e.target.value)}>
                   <option value="">Position</option>
@@ -77,20 +100,35 @@ const Directories = () => {
             {/* Employee cards go here */}
             <div className="card-placeholder">Employee Card</div>
             <div className="card-placeholder">Employee Card</div>
+
+            {filteredEmployees.map((employee) => (
+              <div key={employee.id}>
+                <BioCard
+                  name={employee.basic_info.username}
+                  role={employee.basic_info.role}
+                  email={employee.basic_info.email}
+                  phone={employee.basic_info.phone}
+                  avatar={employee.basic_info.profile_image || ""}
+                  department={employee.department}
+                  location={employee.region}
+                  bio={employee.bio}
+                  status={employee.status}
+                />
+              </div>
+            ))}
           </div>
         </div>
 
         {/* Candidates Section */}
-        <div className="section">
+        {/* <div className="section">
           <div className="section-heading">
             <h2>Candidates</h2>
           </div>
           <div className="card-grid">
-            {/* Candidate cards go here */}
             <div className="card-placeholder">Candidate Card</div>
             <div className="card-placeholder">Candidate Card</div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
